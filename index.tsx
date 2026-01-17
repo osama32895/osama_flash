4790cea82df65025abdaebd2f3059b29fcab3403
Osama ben Naser <157886305+osama32895@users.noreply.github.com>
2026-01-17 23:58:24 +0300

    osama

--- index.tsx
+++ index.tsx
@@ -1,5 +1,5 @@
 
-import '@angular/compiler';
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './src/app.component';
 import { provideZonelessChangeDetection } from '@angular/core';
